e61ef1f8eb9a11078a12d6af6ab82def286e2184
Hari <harikrishkk@gmail.com>
2022-09-29 17:43:04 -0400

    commit 3: connect hasura with react

--- src/index.js
+++ src/index.js
@@ -2,10 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+
+// instantiate a client
+const client = new ApolloClient({
+  uri: 'https://easy-stork-47.hasura.app/v1/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    'content-type': 'application/json',
+    'x-hasura-admin-secret':
+      'fUmKEfiE6thXp8MrhT2r0bRVFFq5THZsB0NI7QFLaxl0xwrkRBOrgscqv7dVkz42',
+  },
+});
+
+client
+  .query({
+    query: gql`
+      query getTodos {
+        todos {
+          done
+          id
+          text
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
